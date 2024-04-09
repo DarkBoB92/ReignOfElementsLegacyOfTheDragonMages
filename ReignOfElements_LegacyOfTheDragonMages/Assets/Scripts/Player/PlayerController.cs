a7f75dc77ab6af2923f8c50f914c597105d045dc
“Roberto Scialpi” <rs9864p@gre.ac.uk>
2024-04-09 16:47:29 +0100

    Player has the ability to transform
    - Added Transform input
    - Need to do Spell Script so able to do extra
       code for PlayerController

--- ReignOfElements_LegacyOfTheDragonMages/Assets/Scripts/Player/PlayerController.cs
+++ ReignOfElements_LegacyOfTheDragonMages/Assets/Scripts/Player/PlayerController.cs
@@ -7,12 +7,14 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] int speed;
-    [SerializeField] GameObject spellPrefab;
+    [SerializeField] GameObject selectedSpell;
+    [SerializeField] GameObject[] spell;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        selectedSpell = spell[0];
     }
 
     // Update is called once per frame
@@ -25,14 +27,38 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 xyInput = input.Get<Vector2>();
         rb.velocity = xyInput * speed;
+        //TODO: - Handle animation depending on direction of where the player is moving
+        //      - Add checks to hold last movement
     }
 
     void OnFire(InputValue input)
     {
         if(input.isPressed)
         {
-            GameObject castedSpeel = Instantiate(spellPrefab, transform.position + transform.right, Quaternion.identity);
-            castedSpeel.GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+            CastSpell(selectedSpell);
+        }
+        //TODO: - Check which direction is walking or last direction pointing so casting will be set on right direction
+    }
+
+    void OnTransform(InputValue input)
+    {
+        if (input.isPressed)
+        {
+            ChangeSpell();
+        }
+    }
+
+    void CastSpell(GameObject currentSpell)
+    {
+        GameObject castedSpeel = Instantiate(currentSpell, transform.position + transform.right, Quaternion.identity);
+        castedSpeel.GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+    }
+
+    void ChangeSpell()
+    {
+        switch(selectedSpell)
+        {
+            //TODO: - Check Spell State and change to the next one
         }
     }
 }
