1578f82f9c0d699c415d3114711b0018606adbd3
“Roberto Scialpi” <rs9864p@gre.ac.uk>
2024-04-16 10:04:25 +0100

    Collectible Completed
    - Tested Singular Scripts
    - Created Prefabs for each item

--- ReignOfElements_LegacyOfTheDragonMages/Assets/Scripts/Items/Collectible.cs
+++ ReignOfElements_LegacyOfTheDragonMages/Assets/Scripts/Items/Collectible.cs
@@ -7,7 +7,7 @@ public class Collectible : MonoBehaviour
     protected Inventory playerInventory;
 
     // When it collides with the Player it gives something to the player and destroys the collectible
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
